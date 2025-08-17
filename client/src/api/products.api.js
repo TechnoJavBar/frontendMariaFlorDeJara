2f5bf5df12a410ea6c9dd6cac13c8ec5f21ca613
xabarjim442 <javibarrancajz@gmail.com>
2025-08-17 13:35:17 +0200

    se ha cambiado la llamada a la api para crear los filtros

--- client/src/api/products.api.js
+++ client/src/api/products.api.js
@@ -4,5 +4,8 @@ const ProductApi = axios.create({
     baseURL: 'https://mariaflordejara.pythonanywhere.com/api/v1/products'
 })
 
-export const getAllProducts = () => ProductApi.get('/');
+export const getAllProducts = (query = "") => {
+    const url = query ? `/?${query}` : "/";
+    return ProductApi.get(url);
+};
 export const getProductById = (id) => ProductApi.get(`/${id}`);
