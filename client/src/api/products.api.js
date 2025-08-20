83717a29bed90e4b7d30a935c095a1bbea08358b
xabarjim442 <javibarrancajz@gmail.com>
2025-08-20 21:02:22 +0200

    se ha cambiado la funcion de obtener todos los productos

--- client/src/api/products.api.js
+++ client/src/api/products.api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const ProductApi = axios.create({
     baseURL: 'https://mariaflordejara.pythonanywhere.com/api/v1/products'
+    // baseURL: 'http://localhost:8000/api/v1/products'
 })
 
-export const getAllProducts = (query = "") => {
-    const url = query ? `/?${query}` : "/";
-    return ProductApi.get(url);
+export const getAllProducts = () => {
+    return ProductApi.get();
 };
 export const getProductById = (id) => ProductApi.get(`/${id}`);
