09f92b00d070e9cbebf32e83fe71af8dcc16d77a
TechnoJavBar <javibarrancajz@gmail.com>
2026-04-03 16:52:00 +0200

    Se ha añadido context de usuarios y carrito además de añadir nuevas rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ProtectedRoute } from "./protectedRoutes.jsx";
+import {CartProvider} from "./context/CartContext.jsx";
+import { AdminRoute } from "./adminRoutes.jsx";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
 import { ProductsPage } from "./pages/productsPage";
@@ -7,27 +10,49 @@ import { ProductsView } from "./pages/productsView";
 import { ContactUs } from "./pages/contactUs";
 import { TermsAndConditionsPage } from "./pages/termsAndConditionsPage";
 import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Perfil } from "./pages/Perfil";
+import {ProductForm} from "./pages/productsForm.jsx";
+import {AdminProductList} from "./pages/adminProductList.jsx";
 import "./App.css";
+import { CartPage } from "./pages/cart.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+    <CartProvider>
       <div className="App container-fluid w-100 p-0">
         <Header />
         <Routes>
           <Route path="/" element={<Navigate to="/Inicio" />} />
           <Route path="/Inicio" element={<IndexPage />} />
           <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:code" element={<ProductsView />} />
+          <Route path="/products/:id" element={<ProductsView />} />
           <Route path="/contacto" element={<ContactUs />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>}/>
           <Route
             path="/TermsAndConditions"
             element={<TermsAndConditionsPage />}
           />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/perfil" element={<Perfil />} />
+            {/* Aquí puedes añadir más: /carrito, /configuracion, etc. */}
+            <Route path="/cart" element={<CartPage/>}/>
+          </Route>
+
+          <Route element={<AdminRoute/>}>
+            <Route path="/admin/productos" element={<AdminProductList/>}/>
+            <Route path="/admin/productos/nuevo" element={<ProductForm/>}/>
+            <Route path="/admin/productos/editar/:id" element={<ProductForm/>}/>
+          </Route>
+          
+
+          
         </Routes>
         <Footer />
       </div>
+      </CartProvider>
     </BrowserRouter>
   );
 }
