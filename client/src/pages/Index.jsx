8ea2b6712e582d440eb5da74897083939d9b04a5
TechnoJavBar <javibarrancajz@gmail.com>
2026-04-03 16:59:05 +0200

    Se ha eliminado texto innecesario

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -6,7 +6,6 @@ export function IndexPage() {
     <div className="index-page container-fluid w-100 min-vh-100">
       <div className="d-flex flex-column">
         <WelcomeDiv />
-        {/* Additional components can be added here */}
       </div>
     </div>
   );
