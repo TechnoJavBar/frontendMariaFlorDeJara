80bb84ce79aed652d95fd75fe6c9e38fabff294b
TechnoJavBar <javibarrancajz@gmail.com>
2026-04-03 16:49:09 +0200

    Se ha creado la configuración del axios

---
+++ client/src/api/axios.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "http://localhost:3000",
+    withCredentials: true,
+    // headers: {
+    //     'Content-Type': 'application/json'
+    // }
+});
+
+api.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if(error.response) {
+            const {status} = error.response;
+
+            if(status === 401)
+            {
+                console.warn("sesion no valida o expirada. Redirigiendo");
+                localStorage.clear('user');
+                
+                window.location.href = '/login';
+                
+            }
+
+            if(status === 403){
+                console.error("No tienes permisos para realizar esta acción");
+                alert("Acceso denegado: se requiere el rol de administrador");
+            }
+        }
+
+        return Promise.reject(error);
+    }
+)
