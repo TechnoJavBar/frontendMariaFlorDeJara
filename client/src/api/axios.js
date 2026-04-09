a057f47231d96d7f2ac3717de8091971417918e8
TechnoJavBar <javibarrancajz@gmail.com>
2026-04-09 19:25:37 +0200

    Se ha actualizado la api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://apimariaflordejara.vercel.app/",
     withCredentials: true,
     // headers: {
     //     'Content-Type': 'application/json'
