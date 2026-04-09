4fea7f072e8f649b738bcbf05e747eaa66ab518d
TechnoJavBar <javibarrancajz@gmail.com>
2026-04-09 11:43:36 +0200

    Se ha modificado las llamadas a la api

--- client/src/api/products.api.js
+++ client/src/api/products.api.js
@@ -1,30 +1,28 @@
 import { api } from "./axios.js";
 
 export const getAllProducts = (page = 1, limit = 8) => {
-    return api.get('/productos', {
-        params: {
-            page: page,
-            limit: limit
-        }
-    });
+  return api.get("/productos", {
+    params: {
+      page: page,
+      limit: limit,
+    },
+  });
 };
 
-export const getLatestProducts =(limit) => {
-    return api.get(`/productos?limit=${limit}`);
-}
+export const searchProductsByName = (nombre) => api.get(`/productos/search/${nombre}`);
 
 export const getProductById = (id) => api.get(`/productos/${id}`);
 
 export const createProduct = (productoData) => {
-    return api.post('/productos', productoData, {
-    headers: { 'Content-Type': 'multipart/form-data' }
-});
-}
+  return api.post("/productos", productoData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
 
 export const updateProduct = (id, productoData) => {
-    return api.put(`/productos/${id}`, productoData);
-}
+  return api.put(`/productos/${id}`, productoData);
+};
 
 export const deleteProduct = (id) => {
-    return api.delete(`/productos/${id}`);
-}
+  return api.delete(`/productos/${id}`);
+};
