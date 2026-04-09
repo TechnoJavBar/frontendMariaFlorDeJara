43748e73d4c70b865887945da1473c17f69289a0
TechnoJavBar <javibarrancajz@gmail.com>
2026-04-09 19:51:11 +0200

    Se ha cambiado la ruta de la api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: "http://localhost:3000",
-    baseURL: "https://apimariaflordejara.vercel.app/",
+    baseURL: "https://apimariaflordejara-188m.vercel.app",
     withCredentials: true,
     // headers: {
     //     'Content-Type': 'application/json'
